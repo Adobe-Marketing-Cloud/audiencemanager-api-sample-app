09823a688159af744d89eb119591366e09a527ef
Harleen Sahni <harleenssahni@gmail.com>
2015-03-07 21:47:03 -0500

    Update exercise-1-get-token.sh
    handle & in passwords when getting token by url encoding

--- exercise-1-get-token.sh
+++ exercise-1-get-token.sh
@@ -5,7 +5,6 @@ read -p 'Audience Manager OAuth2 Client Secret:' client_secret
 read -p 'Audience Manager Username:' user
 read -p 'Audience Manager Password:' password
 
-curl -v --user $client_id:$client_secret -X POST --data "grant_type=password&username=$user&password=$password" https://api.demdex.com/oauth/token
-
+curl -v --user $client_id:$client_secret -X POST --data-urlencode "grant_type=password" --data-urlencode "username=$user" --data-urlencode "password=$password" https://api.demdex.com/oauth/token
 #CURL doesn't output a final newline -- do it here to keep console clean
 echo 
